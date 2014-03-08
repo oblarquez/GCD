95b2ad0dfd46802cab4d61064b3c2d852574d3e6
oblarquez <blarquez@gmail.com>
2014-03-08 12:26:05 -0500

    ready to go to master for reslease

--- R/potveg.R
+++ R/potveg.R
@@ -1,8 +1,25 @@
 potveg=function(ID,classif="RF99",buffer=NULL){
   
+  
+  paleofiresites=NULL
+  # New site location 
+  data(paleofiresites,envir = environment())
+  x=paleofiresites[paleofiresites$ID_SITE %in% ID$SitesIDS,]$LONGITUDE
+  y=paleofiresites[paleofiresites$ID_SITE %in% ID$SitesIDS,]$LATITUDE
+  data<-data.frame(x,y)
+  
+  
   if(classif=="RF99") {
-    data(PNV_RF99);r=PNV_RF99
-  } else {data(PNV_L12);r=PNV_L12}
+    PNV_RF99=NULL
+    data(PNV_RF99,envir = environment())
+    r=raster::crop(PNV_RF99,extent(min(x)-10,max(x)+10,min(y)-10,max(y)+10))
+    
+  } 
+  if(classif=="L12"){
+    PNV_L12=NULL
+    data(PNV_L12,envir = environment())
+    r=raster::crop(PNV_L12,extent(min(x)-10,max(x)+10,min(y)-10,max(y)+10))
+  }
   
   if(classif=="RF99") {
     vnames=c("1: Tropical Evergreen Forest/Woodland",
@@ -18,20 +35,10 @@ potveg=function(ID,classif="RF99",buffer=NULL){
              "12: Open Shrubland","13: Tundra","14: Desert","15: Polar Desert/Rock/Ice")
   } else vnames=c("1: Boreal forest","2: Desert vegetation","3: Grassland and dry shrubland","4: Savannas abd dry woodlands",
                   "5: Temperate forest" ,"6: Tropical forest", "7: Tundra", "8: Warm temperate","9: Warm desert","10: Cold desert")
-  
-  # New site location 
-  capture.output(
-    x <- summary(ID)$LONGITUDE,
-    y <- summary(ID)$LATITUDE,
-    file='NUL')
-  data<-data.frame(x,y)
-  
-  r=crop(r,extent(min(x)-10,max(x)+10,min(y)-10,max(y)+10))
-  
+    
   # points(x,y)
   # Convert data to spatial points data frame using sp
   coordinates(data) <- c("x","y")
-  
   
   
   ## OR within a buffer using density
@@ -40,19 +47,19 @@ potveg=function(ID,classif="RF99",buffer=NULL){
     
     buffer_dens=function(x){
       if(sum(x,na.rm=TRUE)!=0){
-      dens=density(x,na.rm=TRUE,n=25)
-      #plot(density(x,na.rm=TRUE,n=20))
-      pv=round(dens$x[which(dens$y==max(dens$y))])
+        dens=density(x,na.rm=TRUE,n=25)
+        #plot(density(x,na.rm=TRUE,n=20))
+        pv=round(dens$x[which(dens$y==max(dens$y))])
       } else pv=NA
       return(pv)
     }
     
     loc=lapply(buff,buffer_dens)
     
-    loc=unlist(loc)} else
+    loc=unlist(loc)} else loc=extract(r, data)
       
       # Find potveg number at EXACT data location
-      loc=extract(r, data)
+      
   ## Done
   
   data=data.frame(x,y,loc=loc)
@@ -93,6 +100,9 @@ potveg=function(ID,classif="RF99",buffer=NULL){
 }
 
 plot.potveg=function(x,size=4,palette=NULL,alpha=0.5,text=FALSE,...){
+  
+  ## no visible binding for global variable check problem: 
+  y<-name<-veg_id<-NULL
   
   if(is.null(palette))
     pal=c("#8dd3c7","#ffffb3","#bebada","#fb8072",
