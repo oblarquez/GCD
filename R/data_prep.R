9a14674fda2446a36ad4821ebc0bd45894121704
oblarquez <blarquez@gmail.com>
2017-05-04 17:54:48 -0400

    file for preparation of data (internal)

--- R/data_prep.R
+++ R/data_prep.R
@@ -4,45 +4,51 @@ library(paleofire)
 dat=read.csv("/Users/OlivierB/Downloads/charcoals.csv",sep=",",header=T)
 sites=read.csv("/Users/OlivierB/Downloads/cores.csv", sep=",",header=T)
 
-unique(sites$id_site)==unique(dat[,1])
 
 dat$UNIT=NA
 class(dat$UNIT)
-
-length(unique(sites$gcd_access_id))
-
+data(paleofiresites)
 data("paleofiredata")
+
+paleofiredata=paleofiredata[duplicated(paleofiredata)==FALSE,] # Drop duplicates
+paleofiredata=subset(paleofiredata, !is.na(paleofiredata$EST_AGE))
+paleofiredata=subset(paleofiredata, paleofiredata$QUANTITY!=-9999)
+paleofiredata=subset(paleofiredata, paleofiredata$EST_AGE>-100)
+
+dat$est_age_cal_bp[ which(dat$quantity==-9999)]=-9999
+dat1=subset(dat, dat$est_age_cal_bp>-100)
+
+
+
 id=unique(paleofiredata$ID_SITE)
 
+
 for (i in 1:length(id)){
-newid=sites$id_site[sites$gcd_access_id==id[i]]
-dat$UNIT[dat$id_site==newid]<-as.character(paleofiredata$UNIT[paleofiredata$ID_SITE==id[i]])
-cat(i, " ")
+  options(warn=0)
+  newid=sites$id_site[sites$gcd_access_id==id[i]]
+  dat1$UNIT[dat1$id_site==newid]<-as.character(paleofiredata$UNIT[paleofiredata$ID_SITE==id[i]])
+  cat(i, " ")
+}
+
+## Verifier sites 463 ,731, 736
+
+levels(paleofiredata$METHOD)
+levels(dat1$charcoal_method_name)
+
+levels(dat1$charcoal_method_name)<-c("NA", "ACID", "CPRO", "IMAG", "HVLQ", "OREC",  "GRAV", "HNPK",  "NOTK",  "OTHE", "POLS", "SIEV" )
+
+levels(paleofiredata$TYPE)
+
+
+levels(dat1$charcoal_units_name)
+
+lst=list.files('/Users/OlivierB/Documents/Shared/GCD_tables/')
+for( i in 1:length(lst)){
+  yo=tolower(substr(lst[i],1,nchar(lst[i])-4))
+  toto=read.csv(paste0("/Users/OlivierB/Documents/Shared/GCD_tables/",lst[i]))
+  assign(yo,toto)
+  save(file=paste0("/Users/OlivierB/Documents/GCD/data/",yo,".rda"),yo,compress="xz")
 }
 
 
-
-
-a=pfSiteSel(id_site==123)
-char=pfExtract(a)
-
-y=as.vector(pfMinMax(char$QUANTITY))
-x=as.vector(char$EST_AGE)
-win=500
-
-locbootA <- locfit(y ~ lp(x, deg = 1, h = win/2), maxk = 500, family = "qrgauss")
-predbootA <- predict(locbootA, newdata = x, se.fit = TRUE)
-
-plot(x,y)
-lines(x,predbootA$fit)
-
-
-A=read.csv("http://blarquez.com/public/code/loupchar.csv")
-C_=A[,6] # charcoal areas
-P_=A[,1:5] # CmTop, CmBot, AgeTop, AgeBot, Volume 
-
-
-## Calculates charcoal accumulation rate (CHAR, mm2.cm-2.yr-1)
-CHAR=pretreatment(params=P_,serie=C_,Int=TRUE)
-plot(CHAR,xlim=c(12000,0))
 
