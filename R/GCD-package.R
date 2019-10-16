ce340209144b1c97287115d68441d96d9d4efcc4
oblarquez <blarquez@gmail.com>
2019-10-16 14:21:11 -0400

    4.0.5 commits

--- R/GCD-package.R
+++ R/GCD-package.R
@@ -211,23 +211,21 @@ NULL
 #' ## maybe str(paleofiresites) ; plot(paleofiresites) ...
 #' 
 NULL
+
 #' Date table
 #' 
 #' Table with the dating information contained in GCD
 #' 
-#' %% ~~ If necessary, more details than the __description__ above ~~
 #' 
 #' @name date
 #' @docType data
 #' @format A data frame with 4872 observations on the following 8 variables.
-#' \describe{ \item{list("ID_SITE")}{a numeric vector}
-#' \item{list("DATE_LAB_NUMBER")}{a factor with many levels}
-#' \item{list("DEPTH")}{a numeric vector} \item{list("TICKNESS")}{a numeric
-#' vector} \item{list("AGE")}{a numeric vector} \item{list("ERROR")}{a numeric
-#' vector} \item{list("ID_DATE_TYPE")}{a numeric vector}
-#' \item{list("ID_MAT_DATED")}{a factor with levels} }
-#' @references %% ~~ possibly secondary sources and usages ~~
-#' @source %% ~~ reference to a publication or URL from which the data were
+#' \describe{ \item{ID_SITE}{a numeric vector}
+#' \item{DATE_LAB_NUMBER}{a factor with many levels}
+#' \item{DEPTH}{a numeric vector} \item{TICKNESS}{a numeric
+#' vector} \item{AGE}{a numeric vector} \item{ERROR}{a numeric
+#' vector} \item{ID_DATE_TYPE}{a numeric vector}
+#' \item{ID_MAT_DATED}{a factor with levels} }
 #' obtained ~~
 #' @keywords datasets
 #' @examples
@@ -243,17 +241,13 @@ NULL
 #' Material Dated
 #' 
 #' 
-#' %% ~~ If necessary, more details than the __description__ above ~~
-#' 
 #' @name mat_dated
 #' @docType data
 #' @format A data frame with 23 observations on the following 4 variables.
-#' \describe{ \item{list("ID_MAT_DATED")}{a numeric vector}
-#' \item{list("MAT_DATED_STANDARD_LEVEL")}{a character vector}
-#' \item{list("MAT_DATED_TYPE")}{a character vector}
-#' \item{list("MAT_DATED_HIGH_LEVEL")}{a character vector} }
-#' @references %% ~~ possibly secondary sources and usages ~~
-#' @source %% ~~ reference to a publication or URL from which the data were
+#' \describe{ \item{ID_MAT_DATED}{a numeric vector}
+#' \item{MAT_DATED_STANDARD_LEVEL}{a character vector}
+#' \item{MAT_DATED_TYPE}{a character vector}
+#' \item{MAT_DATED_HIGH_LEVEL}{a character vector} }
 #' obtained ~~
 #' @keywords datasets
 #' @examples
@@ -268,17 +262,14 @@ NULL
 #' Type of dates in date.rda
 #' 
 #' 
-#' %% ~~ If necessary, more details than the __description__ above ~~
 #' 
 #' @name date_type
 #' @docType data
 #' @format A data frame with 31 observations on the following 4 variables.
-#' \describe{ \item{list("ID_DATE_TYPE")}{a numeric vector}
-#' \item{list("DATE_TYPE_CODE")}{a character vector}
-#' \item{list("DATE_TYPE_NAME")}{a character vector}
-#' \item{list("DATE_TYPE_NUMBER")}{a numeric vector} }
-#' @references %% ~~ possibly secondary sources and usages ~~
-#' @source %% ~~ reference to a publication or URL from which the data were
+#' \describe{ \item{ID_DATE_TYPE}{a numeric vector}
+#' \item{DATE_TYPE_CODE}{a character vector}
+#' \item{DATE_TYPE_NAME}{a character vector}
+#' \item{DATE_TYPE_NUMBER}{a numeric vector} }
 #' obtained ~~
 #' @keywords datasets
 #' @examples
