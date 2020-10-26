98c1cf8aafea4a23c58dd077aa08960671022df2
Olivier Blarquez <blarquez@gmail.com>
2020-10-26 14:53:54 -0400

    4.0.7

--- R/GCD-package.R
+++ R/GCD-package.R
@@ -181,7 +181,7 @@ NULL
 #' \describe{
 #'   \item{\code{id_site}}{sites primary key from GCD v4.x.x}
 #'  \item{\code{id_site_old}}{sites primary key from GCD v3.x.x, deprecated}
-#'   \item{\code{pref_units}}{a factor with levels \code{\%125} \code{\%DWT} \code{\%TOF} \code{\%WTO} \code{100P} \code{125V} \code{5015} \code{7PRA} \code{8POS} \code{ABSC} \code{ARCC} \code{AREA} \code{ARPG} \code{BCCT} \code{C010} \code{C01K} \code{C100} \code{C120} \code{C125} \code{C140} \code{C150} \code{C250} \code{C550} \code{CARE} \code{CCMY} \code{CG2M} \code{CHCO} \code{CHRX} \code{CM10} \code{CMGR} \code{CMML} \code{COML} \code{CP20} \code{CPAR} \code{CPRA} \code{DC13} \code{FRAG} \code{G05I} \code{G10I} \code{GCHG} \code{GCMY} \code{GRG1} \code{GRGR} \code{GRPC} \code{GT05} \code{GT10} \code{GT12} \code{GT15} \code{GT18} \code{GT24} \code{GT25} \code{GT2X} \code{GT30} \code{GT38} \code{GT50} \code{GT53} \code{GT5C} \code{GT5D} \code{LT12} \code{LT15} \code{LT18} \code{LT1M} \code{LT25} \code{LT40} \code{M2GR} \code{M2ML} \code{MCM3} \code{MM2C} \code{MM2G} \code{MMDW} \code{MMML} \code{MT10} \code{NORM} \code{NOTK} \code{ORDI} \code{OTHE} \code{PCM1} \code{PCMY} \code{PHYT} \code{PP25} \code{PP51} \code{PP52} \code{PPOL} \code{PRAB} \code{PROB} \code{PTC0} \code{PTC2} \code{PTC3} \code{PTCT} \code{SQC0} \code{SQCU} \code{SQCY} \code{SQG5} \code{TOM1} \code{TOM2} \code{TOM3} \code{TOM4} \code{TOM5} \code{TOM6} \code{TOM7} \code{TOM9} \code{TOMX} \code{V125} \code{X010} \code{X01K} \code{X05C} \code{X100} \code{X105} \code{X106} \code{X120} \code{X125} \code{X150} \code{X160} \code{X180} \code{X18C} \code{X20P} \code{X250} \code{X25P} \code{X37P} \code{X459} \code{X512} \code{X520} \code{X550} \code{XA1K} \code{XARE} \code{XARP} \code{XARS} \code{XC25} \code{XCM3} \code{XCOP} \code{XFML} \code{XFRG} \code{XFRP} \code{XFRS} \code{XIMG} \code{XPEA} \code{XPEE} \code{XPIX} \code{XRIF}}
+#'   \item{\code{pref_units}}{a factor, see help("charcoal_units") and data("charcoal_units") for details}
 #'   \item{\code{site_name}}{a character vector}
 #'   \item{\code{lat}}{a numeric vector}
 #'   \item{\code{long}}{a numeric vector}
@@ -194,15 +194,15 @@ NULL
 #'   \item{\code{max_est_age}}{a numeric vector}
 #'   \item{\code{num_samp}}{a numeric vector}
 #'   \item{\code{has_depth}}{factor indicating presence of depths associated to ages}
-#'   \item{\code{date_int}}{a numeric vector}
-#'   \item{\code{qtype}}{a factor with levels \code{CONC} \code{OTHE} \code{INFL} \code{C0P0} \code{NOTK} \code{SOIL}}
-#'   \item{\code{water_depth}}{a character vector}
-#'   \item{\code{depo_context}}{a character vector}
-#'   \item{\code{id_site_type}}{a character vector}
-#'   \item{\code{id_basin_size}}{a character vector}
-#'   \item{\code{id_lands_desc}}{a character vector}
-#'   \item{\code{l12}}{a numeric vector}
-#'   \item{\code{rf99}}{a numeric vector}
+#'   \item{\code{date_int}}{a numeric vector, interval in years between two dates}
+#'   \item{\code{qtype}}{a factor, see help("charcoal_units") and data("charcoal_units") for details}
+#'   \item{\code{water_depth}}{a numeric vector}
+#'   \item{\code{depo_context}}{a character vector, see help("depo_contex") and data("depo_contex") for details}
+#'   \item{\code{id_site_type}}{a character vector, see help("site_type") and data("site_type") for details}
+#'   \item{\code{id_basin_size}}{a character vector, see help("basin_size") and data("basin_size") for details}
+#'   \item{\code{id_lands_desc}}{a character vector, see help("lands_desc") and data("lands_desc") for details}
+#'   \item{\code{l12}}{a numeric vector, Levavasseur et al. 2012}
+#'   \item{\code{rf99}}{a numeric vector, Ramankutty et al. 1999}
 #'   \item{\code{gcd_version}}{main GCD releases}
 #'   \item{\code{num_version}}{a numeric vector: version number 401 meaning 4.0.1}
 #'   \item{\code{update_date}}{date of data update in the GCD}
@@ -285,5 +285,79 @@ NULL
 #' 
 #' data(date_type)
 #' ## maybe str(date_type) ; plot(date_type) ...
+#' 
+NULL
+
+
+
+#' Depositional contexts
+#' 
+
+#' @name depo_context
+#' @docType data
+#' @format A data frame with 17 observations on the following 4 variables.
+#' \describe{ \item{ID_DEPO_CONTEXT}{a numeric vector}
+#' \item{DEPO_CONTEXT_CODE}{a character vector}
+#' \item{DEPO_CONTEXT_NAME}{a character vector}
+#' \item{DEPO_CONTEXT_NUMBER}{a numeric vector} }
+#' @keywords datasets
+#' @examples
+#' 
+#' data(depo_context)
+#' ## maybe str(depo_context) ; plot(depo_context) ...
+#' 
+NULL
+
+
+#' Type of sites
+#' 
+#' @name site_type
+#' @docType data
+#' @format A data frame with 53 observations on the following 4 variables.
+#' \describe{ \item{ID_SITE_TYPE}{a numeric vector}
+#' \item{SITE_TYPE_LEVEL}{a character vector}
+#' \item{SITE_TYPE_DESC}{a character vector}
+#' \item{SITE_TYPE_HIGH_LEVEL}{a character vector} }
+#' @keywords datasets
+#' @examples
+#' 
+#' data(site_type)
+#' ## maybe str(site_type) ; plot(site_type) ...
+#' 
+NULL
+
+
+#' Landscape description elements
+#' 
+#' 
+#' @name lands_desc
+#' @docType data
+#' @format A data frame with 20 observations on the following 3 variables.
+#' \describe{ \item{ID_LANDS_DESC}{a numeric vector}
+#' \item{LANDS_DESC_CODE}{a character vector}
+#' \item{LANDS_DESC_NAME}{a character vector} }
+#' @keywords datasets
+#' @examples
+#' 
+#' data(lands_desc)
+#' ## maybe str(lands_desc) ; plot(lands_desc) ...
+#' 
+NULL
+
+#' Basin size
+#' 
+#' @name basin_size
+#' @docType data
+#' @format A data frame with 7 observations on the following 5 variables.
+#' \describe{ \item{ID_BASIN_SIZE}{a numeric vector}
+#' \item{BASIN_SIZE_CODE}{a character vector}
+#' \item{BASIN_SIZE_DESC}{a character vector}
+#' \item{BASIN_SIZE_RANGE_MIN}{a character vector}
+#' \item{BASIN_SIZE_RANGE_MAX}{a character vector} }
+#' @keywords datasets
+#' @examples
+#' 
+#' data(basin_size)
+#' ## maybe str(basin_size) ; plot(basin_size) ...
 #' 
 NULL
